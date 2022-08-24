fa30a62907ac652912717f68c10b82e6ac83e801
DevAdedeji <adedejitewogbade2@gmail.com>
2022-08-24 20:34:17 +0100

    J

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
    },
    axios: {
      // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-     baseURL: 'https://api.codedevents.com/',
+    //  baseURL: 'https://api.codedevents.com/',
    },
    tailwindcss: {
      jit: true,
